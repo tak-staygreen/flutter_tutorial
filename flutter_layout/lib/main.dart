1a6364ffd3210a4f47a9b0d1b9705f060025ffcb
Tak <64468419+tak-staygreen@users.noreply.github.com>
2021-01-11 03:23:49 +0900

    要素の行列操作。

--- flutter_layout/lib/main.dart
+++ flutter_layout/lib/main.dart
@@ -10,19 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Material Design',
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            color: Colors.blue,
-            width: 300.0,
-            height: 300.0,
-            child: Text('word'),
-            padding: const EdgeInsets.all(50.0),
-            margin: const EdgeInsets.all(100.0),
-            alignment: Alignment.center,
-            transform: Matrix4.rotationZ(0.1),
-          ),
+      title: 'Column & Row',
+      home: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(color: Colors.blue, width: 100, height: 100),
+                Container(color: Colors.red, width: 100, height: 100),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(color: Colors.green, width: 100, height: 100),
+                Container(color: Colors.orange, width: 100, height: 100),
+              ],
+            ),
+          ],
         ),
       ),
     );
