f1f0835c6d9fee8f117e8bce275f88927f891b53
Tak <64468419+tak-staygreen@users.noreply.github.com>
2021-01-12 02:35:00 +0900

    ListView.separatedを使ってリストを表現。

--- flutter_layout/lib/main.dart
+++ flutter_layout/lib/main.dart
@@ -10,30 +10,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var list = [
-      'メッセージ',
-      'メッセージ',
-      'メッセージ',
-      'メッセージ',
+      "メッセージ",
+      "メッセージ",
+      "メッセージ",
+      "メッセージ",
+      "メッセージ",
+      "メッセージ",
+      "メッセージ",
+      "メッセージ",
     ];
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('ListView'),
-        ),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            if (index >= list.length) {
-              list.addAll([
-                'メッセージ',
-                'メッセージ',
-                'メッセージ',
-                'メッセージ',
-              ]);
-            }
-            return _messageItem(list[index]);
-          },
-        ),
-      ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('ListView'),
+            ),
+            body: ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                return _messageItem(list[index]);
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return separatorItem();
+              },
+              itemCount: list.length,
+            )));
+  }
+
+  Widget separatorItem() {
+    return Container(
+      height: 10,
+      color: Colors.orange,
     );
   }
 
@@ -48,11 +53,11 @@ class MyApp extends StatelessWidget {
           style: TextStyle(color: Colors.black, fontSize: 18.0),
         ),
         onTap: () {
-          print('onTap called.');
-        },
+          print("onTap called.");
+        }, // タップ
         onLongPress: () {
-          print('onLongPress called.');
-        },
+          print("onLongTap called.");
+        }, // 長押し
       ),
     );
   }
